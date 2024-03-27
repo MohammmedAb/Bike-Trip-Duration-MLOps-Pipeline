c8e4143e9def437073b63f11fb1575fa5c1e818a
MohammmedAb <m7amd.777@gmail.com>
2024-03-28 01:41:21 +0300

    insert metrices data to db

--- database/sql.py
+++ database/sql.py
@@ -5,6 +5,9 @@ import os
 load_dotenv()
 
 AWS_INSTANCE = os.getenv('AWS_INSTANCE')
+DB_USERNAME = os.getenv('DB_USERNAME')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+db_connection_string = f"host={AWS_INSTANCE} port=5432 dbname='monitoring' user={DB_USERNAME} password={DB_PASSWORD}"
 
 def create_database():
     try:
@@ -38,5 +41,29 @@ def create_predictions_table():
     except Exception as e:
         print(f"error occurred while creating the table: {e}")
 
-create_database()
-create_predictions_table()
+create_metrics_query = """
+CREATE TABLE metrics (
+    id SERIAL PRIMARY KEY,
+    computation_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    r2_score FLOAT,
+    rmse FLOAT,
+    mean_error FLOAT,
+    share_of_missing_values FLOAT,
+    number_of_drifted_features INT
+);
+"""
+def create_metrics_table():
+    try:
+        
+        with psycopg.connect(db_connection_string) as conn:
+            with conn.cursor() as cur:
+                cur.execute(create_metrics_query)
+                print("Table 'metrices' created successfully in the 'monitoring' database")
+    except Exception as e:
+        print(f"error occurred while creating the table: {e}")
+
+# create_database()
+# create_predictions_table()
+create_metrics_table()
+
+    
--- orchestration/batch_metrics_calculation.py
+++ orchestration/batch_metrics_calculation.py
@@ -38,7 +38,7 @@ db_connection_string = f"host={AWS_INSTANCE} port=5432 dbname='monitoring' user=
 def fetch_yesterdays_data():
 
     today = datetime.now().date()
-    yesterday = today - timedelta(days=1)
+    yesterday = today - timedelta(days=2)
     start_of_yesterday = datetime.combine(yesterday, datetime.min.time())
     end_of_yesterday = datetime.combine(yesterday, datetime.max.time())
     query = """
@@ -61,30 +61,92 @@ def fetch_yesterdays_data():
 @task
 def create_current_df(rows):
     data = []
+    main_df = pd.DataFrame()
 
     for row in rows:
-        # Unpack the values from the row
         id, prediction_input, predicted_value, actual_value = row
 
-        # Extract the column names and data from the prediction_input dictionary
-        columns = prediction_input['columns']
+        row_columns = prediction_input['columns']
         row_data = prediction_input['data'][0]
 
-        # Append the id, predicted_value, and actual_value to the row_data
-        row_data.extend([predicted_value, actual_value])
+        row_dict = dict(zip(row_columns, row_data))
 
-        # Create a dictionary with column names as keys and the row data as values
-        row_dict = dict(zip(columns + ['prediction', 'duration'], row_data))
+        
+        row_dict['prediction'] = predicted_value
+        row_dict['duration'] = actual_value
 
-        # Append the row dictionary to the data list
         data.append(row_dict)
 
-    # Create a DataFrame from the data list with desired column names
-    df = pd.DataFrame(data, columns=columns + ['prediction', 'duration'])
-    return df
+        column_order = row_columns + ['duration', 'prediction']
 
+        # Create a DataFrame for the current row with the desired column order
+        df = pd.DataFrame([row_dict], columns=column_order)
 
+        # Concatenate the current row DataFrame to the main DataFrame
+        main_df = pd.concat([main_df, df], ignore_index=True)
 
+    return main_df
+
+@task(log_prints=True)
+def get_reference_df(current_df):
+    print('Getting reference df...')
+    reference_df = pd.read_parquet('s3://mlops-personal-project/reference_dataset/reference_df.parquet')
+
+    reference_df = reference_df.sample(n=len(current_df))
+
+    print('Finished getting the refrence df!')
+
+    return reference_df
+
+@task(log_prints=True)
+def calculate_model_metrics(current_df, reference_df):
+
+    print('Calculating model metrics...')
+
+    column_mapping = ColumnMapping()
+    column_mapping.target = 'duration'
+    column_mapping.prediction = 'prediction'
+    column_mapping.numerical_features = ['distance']
+    column_mapping.categorical_features = ['rideable_type_docked_bike', 'rideable_type_electric_bike', 'member_casual_member']
+
+    model_performance = Report(metrics=[DataDriftTable(), RegressionQualityMetric(), DatasetMissingValuesMetric()])
+    model_performance.run(current_data=current_df, reference_data=reference_df, column_mapping=column_mapping)
+
+    metrics = {
+        'r2_score': model_performance.as_dict()['metrics'][1]['result']['current']['r2_score'],
+        'rmse': model_performance.as_dict()['metrics'][1]['result']['current']['rmse'],
+        'mean_error': model_performance.as_dict()['metrics'][1]['result']['current']['mean_error'],
+        'share_missing_values': model_performance.as_dict()['metrics'][2]['result']['current']['share_of_missing_values'],
+        'number_of_drifted_columns': model_performance.as_dict()['metrics'][0]['result']['number_of_drifted_columns']
+    }
+
+    print(f'Calculated metrics: {metrics}')
+
+    return metrics
+
+@task(log_prints=True)
+def insert_metrics_to_db(metrics):
+    try:
+        with psycopg.connect(db_connection_string) as conn:
+            with conn.cursor() as cur:
+                query = """
+                INSERT INTO metrics (
+                    r2_score, rmse, mean_error, share_of_missing_values,
+                    number_of_drifted_features, computation_time
+                ) VALUES (
+                    %s, %s, %s, %s, %s, %s
+                )
+                """
+                values = (
+                    metrics['r2_score'], metrics['rmse'], metrics['mean_error'],
+                    metrics['share_missing_values'], metrics['number_of_drifted_columns'],
+                    datetime.now()
+                )
+                cur.execute(query, values)
+                conn.commit()
+                print("Metrics inserted successfully.")
+    except Exception as e:
+        print(f"An error occurred while inserting metrics: {e}")
 
 @flow
 def batch_metrics_calculation_flow():
@@ -92,9 +154,13 @@ def batch_metrics_calculation_flow():
     row_yesterday_data = fetch_yesterdays_data()
     current_df = create_current_df(row_yesterday_data)
 
-    
+    reference_df = get_reference_df(current_df)
 
-    print(current_df)
+    metrics = calculate_model_metrics(current_df, reference_df)
+
+    insert_metrics_to_db(metrics)
+
+    # print(current_df)
 
 if __name__ == "__main__":
     batch_metrics_calculation_flow()
